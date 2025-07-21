00afd52d3335b69f0fb16bf56a543398bdac569e
2300030602 <162713572+2300030602@users.noreply.github.com>
2025-07-21 21:56:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/S203/'
-});
+  base: '/S203/'  // 👈 THIS is the most important fix
+})
